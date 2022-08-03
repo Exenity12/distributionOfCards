7685910cb0ac6a901691b8739f36873dc28d2e33
exenity12 <lar-sky.oleg@yandex.ru>
2022-08-03 18:03:26 +0300

    almost done

--- src/App.js
+++ src/App.js
@@ -4,66 +4,79 @@ import './App.css';
 
 const url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
 let idDeckCard;
-let idIsReceived = false
-
+let idIsReceived = false;
 
 function App() {
 
+    const [isDisable, setIsDisable] = useState(true);
+    const [counterBalance, setCounterBalance] = useState();
     const [deckPlayerTwo, setDeckPlayerTwo] = useState([]);
     const [deckPlayerOne, setDeckPlayerOne] = useState([]);
     
     let getADeck = async () => {
         if(idIsReceived) return;
-        console.log("the deck is received")
+        console.log("the deck is received");
+        setIsDisable(false);
         idIsReceived = true;
         const req = await axios.get(url);
         idDeckCard = req.data.deck_id;
+        setCounterBalance(req.data.remaining);
     };
 
     let getCardPlayerOne = async () => {
         if(!idIsReceived) return;
+        setIsDisable(true);
         idIsReceived = true;
-        let urlCard = "https://deckofcardsapi.com/api/deck/" + idDeckCard + "/draw/?count=1"
+        let urlCard = "https://deckofcardsapi.com/api/deck/" + idDeckCard + "/draw/?count=1";
         const resOne = await axios.get(urlCard);
         setDeckPlayerOne([...deckPlayerOne, ...resOne.data.cards]);
+        setCounterBalance(resOne.data.remaining);
+        setIsDisable(false);
     };
 
     let getCardPlayerTwo = useCallback(
         async () => {
+            setIsDisable(true);
             if(!idIsReceived) return;
             idIsReceived = true;
             let urlCard = "https://deckofcardsapi.com/api/deck/" + idDeckCard + "/draw/?count=1"
             const resTwo = await axios.get(urlCard);
             setDeckPlayerTwo([...deckPlayerTwo, ...resTwo.data.cards]);
+            setCounterBalance(resTwo.data.remaining);
+            setIsDisable(false);
         }, [deckPlayerTwo]
     );
 
     let restart = () => {
         setDeckPlayerOne([]);
         setDeckPlayerTwo([]);
-        idIsReceived = false
+        setCounterBalance();
+        setIsDisable(true);
+        idIsReceived = false;
     }
 
 
     useEffect(() => {
-        console.log(deckPlayerOne)
+        console.log(deckPlayerOne);
     }, [deckPlayerOne]);
-
+     
     useEffect(() => {
-        console.log(deckPlayerTwo)
+        console.log(deckPlayerTwo);
     }, [deckPlayerTwo]);
+
 
 
     return (
         <div className="App">
+            <div className='counterBalance'>{counterBalance}</div>
             <div className="distribution">
-                <button onClick={getADeck}>distribution</button>
+                <button id="getADeck" onClick={getADeck}>Start game</button>
             </div>
             <div className="restart">
-                <button onClick={restart}>restart</button>
+                <button onClick={restart} disabled={isDisable}>restart</button>
             </div>
             <div className="playerOne">
-                <button  className='buttonGetCard' onClick={getCardPlayerOne}>playerOne</button>
+                <button  className='buttonGetCard' onClick={getCardPlayerOne} disabled={isDisable}>playerOne</button>
                 <div className="tableCardList">{deckPlayerOne.map(item => 
                     <div key={item.code}>
                         <img className='cardsList' src={item.images.png} alt={item.code}></img>
@@ -71,10 +84,10 @@ function App() {
                 </div>
             </div>
             <div className="playerTwo">
-                <button className='buttonGetCard' onClick={getCardPlayerTwo}>playerTwo</button>
+                <button className='buttonGetCard' onClick={getCardPlayerTwo} disabled={isDisable}>playerTwo</button>
                 <div className="tableCardList">{deckPlayerTwo.map(item => 
                     <div key={item.code}>
-                        <img className='cardsList' src={item.images.png} alt={item.code}></img>
+                        <img className='cardsList' src={item.images.png} alt="qwereqqr"></img>
                     </div>)}
                 </div>
             </div>
